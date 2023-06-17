c0fd9bc6361fa5601178d2906305930f368a6001
Roshan Bhoyar <65328923+roshan0702@users.noreply.github.com>
2023-06-18 00:29:11 +0530

    Add files via upload

---
+++ Even Odd/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your Number");
+
+        int Num = Convert.ToInt32(Console.ReadLine());
+
+        if (Num % 2 == 0)
+        {
+            Console.WriteLine("{0} is Even Number", Num);
+        }
+        else
+        {
+            Console.WriteLine("{0} is Odd Number", Num);
+        }
+    }
+}
