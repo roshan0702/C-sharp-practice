549770696019b5f1d03ecca6902c94282180cf20
Roshan Bhoyar <65328923+roshan0702@users.noreply.github.com>
2023-06-18 00:43:15 +0530

    Add files via upload

---
+++ Number Swap/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num1, num2, swap;
+        Console.WriteLine("Please enter your first number");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter your second number");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        swap = num1;
+        num1 = num2;
+        num2 = swap;
+
+        Console.WriteLine("new num1 = " +  num1);
+        Console.WriteLine("new num2 = " + num2);
+
+    }
+}
