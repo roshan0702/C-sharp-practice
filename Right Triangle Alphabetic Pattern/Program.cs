a4379bb06b399c2f238ddb26107f1eeaa519c4ed
Roshan Bhoyar <65328923+roshan0702@users.noreply.github.com>
2023-06-26 23:39:28 +0530

    Add files via upload

---
+++ Right Triangle Alphabetic Pattern/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number of rows");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\n");
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("{0} ", Convert.ToChar(j + 64));
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
