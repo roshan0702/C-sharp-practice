7ed8dd94398e610aa5d4cbc798c36291320adeaf
Roshan Bhoyar <65328923+roshan0702@users.noreply.github.com>
2023-06-26 23:37:07 +0530

    Add files via upload

---
+++ Pyramid pattern/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number of Rows");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = rows; j > i ; j--) 
+            {
+                Console.Write("   ");
+            }
+            for (int k = 1; k <= i ; k++)
+            {
+                Console.Write(" *    ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
