1a5510f4ab907de64d4d69165f0639d3938075a9
Roshan Bhoyar <65328923+roshan0702@users.noreply.github.com>
2023-06-26 23:40:15 +0530

    Add files via upload

---
+++ Diamond Alphabet Pattern/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number of rows");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\n");
+
+        for (int i = 1; i <= num; i++)
+        {
+            for (int j = 1; j <= num - i; j++)
+            {
+                Console.Write("   ");
+            }
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write("{0}     ", Convert.ToChar(k + 64));
+            }
+            Console.WriteLine("\n");
+        }
+
+        for (int i = num -1; i >= 1; i--)
+        {
+            for (int j = 1; j <= num - i; j++)
+            {
+                Console.Write("   ");
+            }
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write("{0}     ", Convert.ToChar(k + 64));
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
