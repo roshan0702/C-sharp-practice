82ee81e7818abaabcd488a8bfcc52a3175706da7
Roshan Bhoyar <65328923+roshan0702@users.noreply.github.com>
2023-06-26 23:38:14 +0530

    Add files via upload

---
+++ Inverted Right Triangle Number Pattern (Desc)/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number of Rows:");
+
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\n");
+
+        for (int i = num; i > 0; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("{0} ", num - j+1);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
